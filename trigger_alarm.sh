fd6aa04d3c84bc035f62202bc5f4f2e26729b60a
Matthias Jaros <jaros@vesseltracker.com>
2015-02-10 14:03:16 -0500

    initial commit

---
+++ trigger_alarm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ $# -eq 0 ]]; then
+  echo "Filename is missing"
+  exit 1
+fi
+
+FILENAME=$1
+curl -X GET "http://127.0.0.1:5000/alarm/new?filename=${FILENAME}"
---
+++ webserver.py
@@ -0,0 +1,89 @@
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+from flask import request
+
+import RPi.GPIO as GPIO
+import time
+
+from poster.encode import multipart_encode
+from poster.streaminghttp import register_openers
+import urllib2
+
+register_openers()
+
+app = Flask(__name__)
+
+# Template rendering example
+@app.route("/hello")
+@app.route("/hello/<name>")
+def hello(name=None):
+  return render_template('hello.html', name=name)
+
+# JSON response example
+@app.route("/json")
+def json():
+  return jsonify(first_name='John', last_name='Wayne')
+
+#
+# LED examples
+#
+
+LED_PIN = 23 # Set pin number we are using on the GPIO
+ALARM_PIN = 23
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(LED_PIN, GPIO.OUT) # Configure the pin to be used as an output
+GPIO.setup(ALARM_PIN, GPIO.OUT)
+led_on = False
+GPIO.output(LED_PIN, GPIO.LOW) # Initialize the pin with LOW (0=off)
+GPIO.output(ALARM_PIN, GPIO.LOW)
+upload_running = False
+
+@app.route("/led/on")
+def led_on():
+  global led_on
+  led_on = True
+  GPIO.output(LED_PIN, GPIO.HIGH)
+  return jsonify(led_on=led_on)
+
+@app.route("/led/off")
+def led_off():
+  global led_on
+  led_on = False
+  GPIO.output(LED_PIN, GPIO.LOW)
+  return jsonify(led_on=led_on)
+
+@app.route("/led/toggle")
+def led_toggle():
+  global led_on
+  led_on = not led_on
+  GPIO.output(LED_PIN, GPIO.HIGH if led_on else GPIO.LOW)
+  return jsonify(led_on=led_on)
+
+@app.route("/alarm/new")
+def new_alarm():
+  global upload_running
+  response = ""
+  filename = request.args.get("filename")
+  if not filename:
+    return jsonify(message="Filename is missing")
+  if not upload_running:
+    GPIO.output(ALARM_PIN, GPIO.HIGH)
+    datagen, headers = multipart_encode({"image1": open(filename, "rb")})
+    req = urllib2.Request("http://t8.azurewebsites.net/Files/upload", datagen, headers)
+    response = urllib2.urlopen(req).read()
+    time.sleep(1)
+    GPIO.output(ALARM_PIN, GPIO.LOW)
+    return jsonify(message="Alarm created", response=response)
+  else:
+    return jsonify(message="Busy uploading previous video", response="")
+
+if __name__ == "__main__":
+  # This will enable hot reloading, so you don't need to restart each time
+  # after making a change
+  # MAKE SURE TO DISABLE IN PRODUCTION
+  app.debug = True
+
+  app.run(host='0.0.0.0')
+
